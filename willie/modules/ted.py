ef50e9670960f7963116018c9626875fad67ec9d
Gabe Ortiz <gabe@signalnine.net>
2015-10-31 16:31:31 -0700

    more realistic replies

--- willie/modules/ted.py
+++ willie/modules/ted.py
@@ -3,19 +3,23 @@ import random
 
 responses = ['k', 'rekt', 'owned', 'lol', 'pls', 'no u', 'pls no', 'wat', 'fak', 'pics', 'rip', 'pls stop', 'y', 'k', 'dicks', 'k']
 
-rare_responses = ['what ru jerking off to?', 'fucking rekt', 'weed++', 'sex++', 'your.mom++', 'dongs++', 'pussy++', 'bots++', 'humans--', 'm\'lady']
+rare_responses = ['node.js++', 'fucking rekt', 'php++', 'golang++', 'uber++', 'travis.kalkanicks.sweaty.nutsack++', 'visual.basic++', 'microsoft.word++', 'rust--', 'm\'lady']
 
-rare_replies = ['suck a dick, ', '--', '++', '/me tips his fedora at ']
+rare_replies = ['hi, ', ':( ']
+
+random_karma = ['--', '++']
 
 @module.rule('[a-z]+')
 def chat(bot, trigger):
-    i = random.randint(1, 100)
-    if i < 8 :
+    i = random.randint(1, 200)
+    if i < 7 :
         bot.say(random.choice(responses))
-    if (i > 96) and (i < 99) 
+    if (i > 96) and (i < 99) :
         bot.say(random.choice(rare_responses))
-    if i == 100
+    if i == 100 :
         bot.say(random.choice(rare_replies) + trigger.nick)
+    if i == 95 :
+        bot.say(trigger.nick + random.choice(random_karma))
 
 @module.rule('ted|wexler|abotinacan|amaninacan')
 def nickchat(bot, trigger):
